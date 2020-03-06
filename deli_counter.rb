4a6b1a11c9e3f7021bb5c419f1cb7b4e7e0bef99
James Dodds <dodds1990@gmail.com>
2020-03-06 03:10:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 def line(deli_line)
-  if deli_line.length == 1
+  if deli_line.length == 0
     puts "The line is currently empty."
   else
     output = "The line is currently:"
