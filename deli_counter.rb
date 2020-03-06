83b236fdbab9e71d121ad33bdffedcf2b8fb7440
James Dodds <dodds1990@gmail.com>
2020-03-06 03:03:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,10 +2,11 @@
 def line(deli_line)
   if deli_line.length < 1
     puts "The line is currently empty."
-  # else
-  #   output = "The line is currently: "
-  #   deli_line.each_with_index do |name, place|
-  #     output += "#{place + 1}. #{name}"
-  #   end
+  else
+    output = "The line is currently: "
+    deli_line.each_with_index do |name, place|
+      output += "#{place + 1}. #{name}"
+    end
+    puts output
   end
 end
