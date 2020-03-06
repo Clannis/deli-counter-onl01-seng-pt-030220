6f8d19eb1d176f8ff1535aa4681969f23fd523fb
James Dodds <dodds1990@gmail.com>
2020-03-06 02:57:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+def line(deli_line)
+  if deli_line.length == 0
+    return "The line is currently empty."
+  else
+    output = "The line is currently: "
+    deli_line.each_with_index do |name, place|
+      output += "#{place + 1}. #{name}"
+    end
+  end
+end
