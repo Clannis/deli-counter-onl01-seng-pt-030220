0317611c78c838654c40c06d64e4d676a96d94e9
James Dodds <dodds1990@gmail.com>
2020-03-06 03:09:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 def line(deli_line)
-  if deli_line.length < 1
+  if deli_line.length == 1
     puts "The line is currently empty."
   else
     output = "The line is currently:"
@@ -14,4 +14,13 @@ end
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
+  end
 end
